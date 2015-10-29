1f185809ef74a187eb0c841eaed821a1d5cc91e4
rkgilbert10 <rkgilbert10@gmail.com>
2015-10-29 23:05:02 +0000

    SLNode.cpp complete

---
+++ Challenge19/SLNode.cpp
@@ -0,0 +1,68 @@
+/*
+ * Programming Challenge 19
+ *
+ * Rachel Gilbert
+ * 
+ * Created: 10/29/15
+ * Last Modified: 10/29/15
+ * Notes:
+ * Sources used: 
+ */
+ 
+ #include "SLNode.h"
+ 
+
+ 
+ /* Default constructor
+  * Sets values nextNode(NULL) and contents(0)
+  */
+SLNode::SLNode() {
+    nextNode = NULL;
+    contents = 0;
+}
+
+/* Overloaded constructor
+ * @parameter newContents set to contents
+ * Sets nextNode to NULL;
+ */
+ SLNode::SLNode(int newContents) {
+     contents = newContents;
+     nextNode = NULL;
+ }
+ 
+/* Destructor 
+ * sets nextNode to NULL
+ *
+ */
+ SLNode::~SLNode() {
+     nextNode = NULL;
+ }
+ 
+ /* setContents
+  * @param newContents sets the new contents to contents
+  */
+ void SLNode::setContents (int newContents) {
+   contents = newContents;
+ }
+ 
+ /* getContents
+  * @returns contents
+  */
+ 
+ int SLNode::getContents() const {
+  return contents;
+ }
+ 
+ /* setNextNode
+  * @param newNode sets new node to nextNode by reference
+  */
+ void SLNode::setNextNode(SLNode* newNode) {
+   nextNode = newNode;
+ }
+ 
+ /* getNextNode
+  * @return nextNode
+  */
+ SLNode* SLNode::getNextNode () const {
+  return nextNode;
+ }
